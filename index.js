4670c9d1c6fb41d9466db839d454dc189b62cf45
Shane S. Anderson <s@ander.nz>
2016-07-13 13:22:08 +1200

    Added an optional name filter.

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
-var gulp = require("gulp");
-var exec = require("child_process").execSync;
+function runGitLs() {
+  var exec = require("child_process").execSync;
 
-module.exports = function() {
   var output = "";
   output += exec("git ls-files -c");
   output += exec("git ls-files -o --exclude-standard");
   output = output.trim();
 
   var files = output.split(/\r?\n/g);
+  return files;
+}
+
+function applyFilter(files, optionalRegexp) {
+  if (!optionalRegexp)
+    return files;
+
+  return files.filter(function(name) {
+    return optionalRegexp.test(name);
+  });
+}
+
+function createVfs(files) {
+  var gulp = require("gulp");
   return gulp.src(files);
+}
+
+exports.src = function(optionalRegexp) {
+  var files = runGitLs();
+
+  files = applyFilter(files, optionalRegexp);
+
+  var stream = createVfs(files);
+  return createVfs(files);
 };
